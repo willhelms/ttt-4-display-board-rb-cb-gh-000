792bac9f5cc307951389d0a96fc2deba87cf4fc8
william <william060@student.itb.ac.id>
2018-06-24 04:37:52 +0000

    automatically backed up by learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,2 +1,17 @@
 # Define display_board that accepts a board and prints
 # out the current state.
+def display_board(board)
+  puts row1();
+  puts row2();
+  puts row1();
+  puts row2();
+  puts row1();
+end
+
+def row1()
+  return  "   |   |   "
+end
+
+def row2()
+  return "-----------"
+end
