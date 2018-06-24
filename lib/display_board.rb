55df5a21d7b808bf9cc62da7d7d929fa99d807a3
william <william060@student.itb.ac.id>
2018-06-24 04:48:33 +0000

    Done.

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,2 +1,17 @@
 # Define display_board that accepts a board and prints
 # out the current state.
+def display_board(board)
+  puts row1(board, 0);
+  puts row2();
+  puts row1(board, 3);
+  puts row2();
+  puts row1(board, 6);
+end
+
+def row1(board, num=0)
+  return  " #{board[0 + num]} | #{board[1 + num]} | #{board[2 + num]} ";
+end
+
+def row2()
+  return "-----------";
+end
